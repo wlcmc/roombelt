f701a840ae34bb622940852c3c6ed8de1ca6e07b
Mateusz Zieliński <mateusz.mzielinski@gmail.com>
2018-10-13 23:05:43 +0200

    Fix build

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const app = require("./backend");
+const express = require("express");
 
 if (process.env["NODE_ENV"] === "development") {
   const proxy = require("http-proxy-middleware");
-
-  app.use('/*', proxy({ target: "http://localhost:3001", changeOrigin: true }));
-} else {
-  const path = require("path");
-  const express = require("express");
-
-  app.use(express.static(path.join(__dirname, "frontend/", "build")));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-  });
+  app.use("/*", proxy({ target: "http://localhost:3001", changeOrigin: true }));
 }
+
+app.use(express.static("frontend/build"));
+app.get("/*", (req, res) => res.sendFile("frontend/build/index.html", { root: __dirname }));
