cc9f4756b4c37342ce6065c41e2ad78095c50272
Mateusz Zieliński <mateusz.mzielinski@gmail.com>
2018-10-13 23:30:15 +0200

    Cache all static resources

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -3,6 +3,6 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox
 if (workbox) {
   console.log("Installing service worker");
 
-  workbox.routing.registerRoute(/\/static\/.*\.js/, workbox.strategies.networkFirst());
+  workbox.routing.registerRoute(/\/static\//, workbox.strategies.networkFirst());
   workbox.routing.registerRoute('/device', workbox.strategies.networkFirst());
 }
